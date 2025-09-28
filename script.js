0f7362e1251a717f0460a57d2749f7350e46188e
markmuturi <muturimark16@gmail.com>
2025-09-28 11:21:42 +0300

    update index.html, script.js, styles.css

---
+++ script.js
@@ -0,0 +1,11 @@
+function greet (name) {
+    let name = 'Stranger';
+    console.log ("Hello {`$!name`}");
+};
+
+function sum (num1, num2) {
+    sum = num1 + num2;
+    return sum; 
+    console.log(sum);
+};
+sum (1, 2);
